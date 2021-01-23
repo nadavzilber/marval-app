ceef1e48eaedd809aad991820e06178fdc952726
nadavzilber <nadavzilber@gmail.com>
2021-01-23 17:44:45 +0200

    styling

--- client/src/auth/AuthenticatedApp.js
+++ client/src/auth/AuthenticatedApp.js
@@ -4,6 +4,10 @@ import { useRecoilState } from 'recoil';
 import { getUserInfo, getComics } from '../api';
 import Comics from '../components/Comics';
 import Analytics from '../components/Analytics';
+import Octopus from '../assets/octopus.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 const AuthenticatedApp = () => {
     const [userData, setUserData] = useRecoilState(userAtom);
@@ -33,21 +37,29 @@ const AuthenticatedApp = () => {
     }
 
     return (
-        <>
-            <h4>AuthenticatedApp</h4>
-            <button onClick={(e) => logOut(e)}>Log Out</button>
+        <div className="authenticated-app">
+            <div className="authenticated-app-header">
+                <FontAwesomeIcon icon={faSignOutAlt} className="log-out-button" onClick={(e) => logOut(e)}/>
+                {/* <img className="octopus-logo" src={Octopus} /> */}
+                <div className="app-title">Authenticated App</div>
+                <FontAwesomeIcon icon={faUserCircle} className="user-button" onClick={(e) => console.log(e)}/>
+            </div>
+
+            <div className="button-group">
+                <button onClick={(e) => onGetUserInfo(e)}>Get My User Info</button>
+                <button onClick={(e) => onGetComics(e)}>Get Marval Comics</button>
+            </div>
 
             <h2>id: {userData.id ? userData.id : userData._id}</h2>
             <h2>email: {userData.email}</h2>
             <h2>password: {userData.password}</h2>
 
-            <button onClick={(e) => onGetUserInfo(e)}>Get My User Info</button>
-            <button onClick={(e) => onGetComics(e)}>Get Marval Comics</button>
+
 
             <Analytics />
 
             {!!comics && <Comics data={comics} />}
-        </>)
+        </div>)
 }
 
 export default AuthenticatedApp;
